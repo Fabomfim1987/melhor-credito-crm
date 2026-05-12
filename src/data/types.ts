b93fa0ac06a9f1f5344b575e8f89c2f69d4df9fc
Fabomfim1987 <fabricio.bomfim@live.com>
2026-05-12 18:47:17 -0300

    Add files via upload

---
+++ src/data/types.ts
@@ -0,0 +1,4 @@
+export type Status = 'Ativo' | 'Parou' | 'Migrou' | 'Venda Fraca' | 'Em Negociação' | 'Lista Transmissão'
+export type Empresa = 'NOVA' | 'GLM' | 'LEV' | 'LHAMAS'
+export type Sala = 'PEDRO' | 'VALERIA' | 'ERALDO' | 'GLM' | 'LEV'
+export type Perfil = 'SÓLIDO' | 'CONFIÁVEL' | 'OPORTUNISTA' | 'NÃO CONFIÁVEL' | 'NOVO'
